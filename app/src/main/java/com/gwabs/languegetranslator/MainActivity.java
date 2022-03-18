2e94002d7471cecefeab15ec5084202d6b471ae7
gwabstech <72021618+gwabstech@users.noreply.github.com>
2022-03-18 15:38:54 +0100

    Initial commit

---
+++ app/src/main/java/com/gwabs/languegetranslator/MainActivity.java
@@ -0,0 +1,18 @@
+package com.gwabs.languegetranslator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void sendTranslation(){
+
+    }
+}
